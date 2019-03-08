f793aacf89ea2ab7077c5f53c5222a777d396912
r3muxd <46062054+r3muxd@users.noreply.github.com>
2019-03-07 21:23:52 -0500

    Update xsserthing.js

--- xsserthing.js
+++ xsserthing.js
@@ -1,2 +1,3 @@
 // it's PROBABLY not going to get any of us permabanned right
-var audio = new Audio('https://s.put.re/rJsPWpXZ.mp3'); audio.play();
+var audio = new Audio('https://s.put.re/rJsPWpXZ.mp3')
+audio.play();
